71dd73f16e1d137b5a542ea01546e8b84d29e40b
JD_Rodrigues <jdevproject01@gmail.com>
2022-05-27 17:43:42 -0300

    Adição da página About Us nas rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import { Routes, Route } from 'react-router-dom';
+import AboutUsPage from './components/AboutUsPage';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
          <Routes>
            <Route path="/" element={ <Home /> } />
+           <Route path="/about-us" element={ <AboutUsPage /> } />
            <Route path="/login" element={ <Login /> } />
+           <Route path="/signup" element={ <SignUp /> } />
            <Route path="/footer" element={<Footer /> }/>
          </Routes>
   );
